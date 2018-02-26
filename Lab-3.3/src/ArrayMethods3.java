777427623686f229f67de3d27fb69bba9cb1d508
mss <mss@192.168.2.12>
2018-02-25 23:34:24 -0500

    mergesort and partition done

---
+++ Lab-3.3/src/ArrayMethods3.java
@@ -0,0 +1,101 @@
+import java.util.Arrays;
+
+public class ArrayMethods3 {
+	
+	public static void main(String[] args) {
+		String[] list1 = {"train", "xyz", "saurabh", "computer", "vex", "yeah", "zebra", "hat", "banana", "water"};
+		int[] list3 = {4, 1, 7, 8, 6, 3, 5, 2};
+		
+		//mergeSort Test
+		long start = System.nanoTime();
+		String[] mergeResult = mergeSort(list1);
+		long end = System.nanoTime();
+		long time = end - start;
+		System.out.println("Merge test took: " + time + " nanoseconds");
+		System.out.println(Arrays.toString(mergeResult));
+		
+		//quickSort Test
+		start = System.nanoTime();
+		quickSort(list3, 0, list3.length - 1);
+		end = System.nanoTime();
+		time = end - start;
+		System.out.println("Partition test took: " + time + " nanoseconds");
+		System.out.println(Arrays.toString(list3));
+	}
+	
+	public static String[] mergeSort(String[] list) {
+		
+		/*Write a method that recursively separates the list into two halves and calls mergeSort on both
+		halves. When we reach two halves that have length 1, then merge the arrays together. Return the fully
+		sorted array.*/
+		
+		if (list.length > 1) {
+			String[] l1 = new String[list.length / 2];
+			for (int x = 0; x < l1.length; x++) {
+				l1[x] = list[x];
+			}
+			String[] l2 = new String[list.length - (list.length / 2)];
+			for (int x = 0; x < l2.length; x++) {
+				l2[x] = list[x + list.length / 2];
+			}
+			return merge(mergeSort(l1), mergeSort(l2));
+		}
+		else {	
+			return list;
+		}
+	}
+	
+	public static void quickSort(int[] list1, int front, int back) {
+		if (front != back) {
+			quickSort(list1, front, partition(list1, front, back) - 1);
+			quickSort(list1, partition(list1, front, back) + 1, back);
+		}
+	}
+	
+	public static String[] merge(String[] list1, String[] list2) {
+
+		int x = 0;
+		int y = 0;
+		int xcompy = 0;
+		String[] list3 = new String[list1.length + list2.length];
+		for (int z = 0; z < list3.length; z++) {
+			if (x >= list1.length) {
+				xcompy = 1;
+			}
+			else if (y >= list2.length) {
+				xcompy = -2;
+			}
+			else {
+				xcompy = list1[x].compareTo(list2[y]);
+			}
+			if (xcompy <= 0) {
+				list3[z] = list1[x];
+				x++;
+			}
+			else {
+				list3[z] = list2[y];
+				y++;
+			}
+		}
+		return list3;	
+	}
+	
+	public static int partition(int[] list, int front, int back) {
+		
+		int i = 1;
+		int pivot = list[front];		
+		for (int j = front + 2; j <= back; j++) {
+			if (list[j] < pivot) {
+				i++;
+				int m = list[i];
+				list[i] = list[j];
+				list[j] = m;
+			}
+		}
+		for (int a = 0; a < i; a++) {
+			list[a] = list[a + 1];
+		}
+		list[i] = pivot;
+		return i;
+	}
+}
